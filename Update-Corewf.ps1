e01924fff7eb9cb2a5c92c4f2ae65a9bb8013e19
Alex Mizuki <alex.mizuki@gmail.com>
2020-06-15 18:06:55 -0400

    download nuget package instead

--- Update-Corewf.ps1
+++ Update-Corewf.ps1
@@ -1,11 +1,8 @@
 Remove-Item ../corewf -Recurse -Force -ErrorAction SilentlyContinue
-Push-Location ../
-git clone https://github.com/UIPath-Open/corewf.git
-$csprojPath = (Resolve-Path 'corewf/src/CoreWf/System.Activities.csproj').Path
-$csproj = Get-Content $csprojPath
-$csproj = $csproj -replace "netcoreapp3.1;", "" -join "`r`n"
-#$csproj = $csproj -replace "Exec Command", "Message Text"
-[IO.File]::WriteAllText($csprojPath, $csproj)
-Set-Location corewf/src
-dotnet build ./System.Activities.sln
+New-Item ../corewf -ItemType Directory
+Push-Location ../corewf
+$Archive = 'System.Activities.zip'
+Invoke-WebRequest https://www.myget.org/F/uipath-dev/api/v2/package/System.Activities/3.0.0-20200310-05 -OutFile $Archive
+Expand-Archive $Archive
+Remove-Item $Archive
 Pop-Location
