3dbf1b3cfd8ddadde26dcc7c2d68517c0f2efa06
Alex Mizuki <alex.mizuki@gmail.com>
2020-06-15 15:18:49 -0400

    iniital

---
+++ Scratch/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Scratch
+{
+    public class Class1
+    {
+        public void RunXamlWorkflow()
+        {
+            var xaml = @"<Activity 
+ x:Class=""WorkflowConsoleApplication1.HelloWorld""
+ xmlns=""http://schemas.microsoft.com/netfx/2009/xaml/activities""
+ xmlns:x=""http://schemas.microsoft.com/winfx/2006/xaml"">
+  <Sequence>
+    <WriteLine Text=""Hello World!"" />
+  </Sequence>
+</Activity>";
+            Console.WriteLine(xaml);
+        }
+    }
+}
